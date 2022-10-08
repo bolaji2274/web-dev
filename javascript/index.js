1f61f957411d74535894ea0d01071167758cfcee
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-10-08 15:43:50 +0100

    file js modified

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,128 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+// document.getElementById("myButton").onclick = function(){
+//     var myName = document.getElementById("myText").value;
+//     console.log("Hello",myName);
+// }
 
-    <!-- <label for="">Enter your name: </label> <br>
-    <input type="text" id="myText" placeholder="Your Name"> <br>
-    <button type="button" id="myButton">Submit</button> -->
-    
+function hello(name){
+    console.log(`Hello mr ${name}, how are you doing`);
+};
 
-    <script src="index.js"></script>
-</body>
-</html>
+hello("Bolaji Hammed");
+
+var human = {
+    name: "Bolaji",
+    age: 20,
+    description: function(){
+        console.log("Mr Bolaji is a nice guy");
+    },
+    background: function(){
+        console.log(`${human.name} is from Town A`)
+    },
+    living: function(){
+        console.log("He is living at Osun state")
+    }
+};
+
+console.log(human.background());
+
+var bolaji1 = {
+    firstName: "Bolaji",
+    lastName: "Hammed",
+    age: function(nums){
+        if(nums > 18){
+            console.log(`${bolaji1.firstName} is an adult`);
+        }
+        else{
+            console.log(`${bolaji1.firstName} is still young`);
+        }
+    }
+}
+
+// console.log(bolaji1.age(18));
+var surname = ["Okiki","Olanrewaju","Adeyemo","Olasupo","Olaitan"];
+
+// console.log(surname);
+
+let lens = surname.length;
+
+var names = ["Bolaji","Hammed","Alabi","Opeyemi","Oke"];
+// console.log(names);
+var nums = names.length
+
+for (let i = 0; i < lens; i++){
+    names[(nums) + i] = surname[i];
+}
+
+console.log(names);
+
+let num = names.length;
+for (i = 0; i < num; i++){
+    console.log(names[i])
+}
+
+
+var number = 22;
+if (number > 23){
+    console.log(`${number} is greater than 23`);
+}
+else if (number == 23){
+    console.log(`${number} is equal to 23`);
+}
+else {
+    console.log(`${number} is less than 23`);
+}
+
+var grade = "C";
+
+switch (grade){
+    case "A":
+        console.log("You did great");
+        break;
+
+    case "B":
+        console.log("You did good");
+        break;
+    case "C":
+        console.log("You did okay");
+        break;
+    case "D":
+        console.log("You .. . did not do that well");
+        break;
+    case "F":
+        console.log("YOU FAILED!");
+        break;
+    default:
+        console.log(grade,"is not a letter grade");
+}
+//&& and || or
+var temperature = 11;
+if (temperature > 30){
+    console.log("It's HOT outside");
+}
+else if (temperature > 12 && temperature == 15){
+    console.log("Temperature is in normal condition");
+}
+else {
+    console.log("Temperature is cold");
+}
+
+
+var myName = window.prompt("Enter your name");
+
+var lengthOfName = myName.length
+console.log(lengthOfName);
+
+var list = myName.split(' ')
+
+console.log(list.length)
+
+console.log(`My name is ${myName}`);
+
+var myNumber = window.prompt("Enter your number seperated with comma");
+var myNumberLength = myNumber.split(',');
+var myNumberSort = myNumberLength.sort()
+var lengthOfMyNumber = myNumberLength.length;
+
+for (let i = 0; i < lengthOfMyNumber; i++){
+    console.log(myNumberSort[i]);
+}
