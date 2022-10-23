4055e8e1c7af25f9ee9ab2ca8b79c5697549369a
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-10-23 17:01:38 +0100

    campus and cta modified

--- portfolio/edu vue-project/src/components/Campus.vue
+++ portfolio/edu vue-project/src/components/Campus.vue
@@ -1,7 +1,3 @@
-<style>
-
-</style>
-
 <template>
     <div>
                     <!-- campus -->
@@ -11,13 +7,13 @@
             adispisciing elit.
         </p>
         <div class="row">
-            <div class="campus-col">
-                <img src="london.png" alt="images">
+            <div class="campus-col" v-for="item in items" :key="item">
+                <img :src="item.image" alt="images">
                 <div class="layer">
-                    <h3>Erin Osun</h3>
+                    <h3>{{item.location}}</h3>
                 </div>
             </div>
-            <div class="campus-col">
+            <!-- <div class="campus-col">
                 <img src="newyork.png" alt="images">
                 <div class="layer">
                     <h3>Erin Osun @ Hub</h3>
@@ -28,11 +24,25 @@
                 <div class="layer">
                     <h3>Erin Osun @OKe Awesin</h3>
                 </div>
-            </div>
+            </div> -->
         </div>
     </section>
     </div>
 </template>
+<script>
+export default {
+    data () {
+        return {
+            items: [
+                {image:'london.png',location:'Erin Osun'},
+                {image:'newyork.png',location:'Erin Osun @ Hub'},
+                {image:'washington.png',location:'Erin Osun @ Oke Awesin'}
+
+            ]
+        }
+    }
+}
+</script>
 
 <style>
 
