088539a72ff0a8072139db812b17eea119c988f1
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-10-23 15:36:00 +0100

    vue edu project

---
+++ portfolio/edu-vue-project/src/components/Navbar.vue
@@ -0,0 +1,45 @@
+
+<template>
+<div>
+
+ <section class="header">
+         <nav>
+            <a href="index.html"><img src="logo.png" alt=""></a>
+            <div class="nav-links">
+                <i class="fa fa-times"></i>
+                <ul>
+                    <li><a href="">HOME</a></li>
+                    <li><a href="">ABOUT</a></li>
+                    <li><a href="">COURSE</a></li>
+                    <li><a href="">BLOG</a></li>
+                    <li><RouterLink to="/course">Course</RouterLink></li>
+                    <li><router-link to="/contact">Contact us</router-link></li>
+                    <li><a href="">CONTACT</a></li>
+                </ul>
+            </div>
+            <i class="fa fa-bars"></i>
+         </nav>
+    
+<div class="text-box">
+    <h1>ELERINMOSA UNIVERSITY OF TECHNOLOGY </h1>
+    <P>This is a website for ELERINMOSA UNIVERSITY OF TECHNOLOGY 
+        located in Erin Osun. The founder was Coxxxx 
+        and led by Cohortxxxx learning <br> 
+        all different kind of course in all aspect of study
+    </P>
+    <a href="" class="hero-btn">Visit Us To Know More About Our Services</a>
+</div>
+    </section>
+    <RouterView /> 
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
