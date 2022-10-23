66b87ad8eb116197fbd1b03823cd2eddb015494a
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-10-23 16:28:26 +0100

    file added.

--- portfolio/edu vue-project/src/components/Navbar.vue
+++ portfolio/edu vue-project/src/components/Navbar.vue
@@ -20,11 +20,7 @@
     
 <div class="text-box">
     <h1>{{headMessage}}</h1>
-    <P>This is a website for ELERINMOSA UNIVERSITY OF TECHNOLOGY 
-        located in Erin Osun. The founder was Coxxxx 
-        and led by Cohortxxxx learning <br> 
-        all different kind of course in all aspect of study
-    </P>
+    <P></P>
     <a href="" class="hero-btn">Visit Us To Know More About Our Services</a>
 </div>
     </section>
@@ -37,6 +33,7 @@ export default {
     props: ['headMessage'],
     data(){
         return{
+            desc:'This is a website for ELERINMOSA UNIVERSITY OF TECHNOLOGY located in Erin Osun. The founder was Coxxxx and led by Cohortxxxx learning <br>all different kind of course in all aspect of study',
             links: [
                 {name: 'HOME'},
                 {name: 'ABOUT'},
