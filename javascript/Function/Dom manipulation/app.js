0d34e747936e08d835d53df0bd432e52233880f0
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-10-28 22:21:00 +0100

    File doms modified

--- javascript/Function/Dom manipulation/app.js
+++ javascript/Function/Dom manipulation/app.js
@@ -1,2 +1,11 @@
-var myId = document.getElementById('min-heading');
+const myId = document.getElementById('min-heading');
 console.log(myId);  
+
+const myClass = document.getElementsByClassName('list-items');
+console.log(myClass);
+
+const listItem = document.getElementsByTagName('li');
+console.log(listItem);
+
+const container = document.querySelector('div');
+console.log(container);
