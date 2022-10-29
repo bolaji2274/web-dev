145ba3df2221b08cdc1b715c7aa9419e8ab4eef7
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-10-29 17:57:13 +0100

    changes made to app.js

--- javascript/Function/Dom manipulation/app.js
+++ javascript/Function/Dom manipulation/app.js
@@ -1,14 +1,14 @@
-const myId = document.getElementById('min-heading');
-console.log(myId);  
+// const myId = document.getElementById('min-heading');
+// console.log(myId);  
 
-const myClass = document.getElementsByClassName('list-items');
-console.log(myClass);
+// const myClass = document.getElementsByClassName('list-items');
+// console.log(myClass);
 
-const listItem = document.getElementsByTagName('li');
-console.log(listItem);
+// const listItem = document.getElementsByTagName('li');
+// console.log(listItem);
 
-const container = document.querySelector('div');
-console.log(container);
+// const container = document.querySelector('div');
+// console.log(container);
 
-const container1 = document.querySelectorAll('div');
-console.log(container1);
+// const container1 = document.querySelectorAll('div');
+// console.log(container1);
