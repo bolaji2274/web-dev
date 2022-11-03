b88b5141264582a3fe4e4dc42cc306a79c070ad5
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-11-03 12:27:03 +0100

    Two file modified

--- vue-todo-app/src/components/HelloWorld.vue
+++ vue-todo-app/src/components/HelloWorld.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Hello World</h1>
+
 </template>
 
 <script>
 export default {
-  
+  name: 'app',
+  components: {
+    
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Todo One',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Todo two',
+          completed: true
+        },
+        {
+          id: 3,
+          title: 'Todo three',
+          completed: false
+        }
+      ]
+    }
+  },
 }
-</script>
+</script>  
