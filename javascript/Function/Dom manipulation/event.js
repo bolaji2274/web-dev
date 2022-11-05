90785e25f20bed22d23b97622581992fa778226a
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-11-05 08:38:23 +0100

    add event modifiers

---
+++ javascript/Function/Dom manipulation/event.js
@@ -0,0 +1,14 @@
+const btnTwo = document.querySelector('.btn-2');
+console.log(btnTwo);
+
+// btnTwo.addEventListener("click", () => {
+//     alert("Bolaji also love javascript");
+// })
+
+// or 
+
+function alertBtn(){
+    alert("I also love javascript Bro");
+}
+btnTwo.addEventListener("click", alertBtn);
+
