ebf040dad9cd0126da3a8910491e7c264e8b9e1a
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-11-07 09:25:42 +0100

    two files modified

--- vue-app-todo/src/views/HomeView.vue
+++ vue-app-todo/src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup>
-
+<script>
+import TodoVue from '../components/Todo.vue';
+export default{
+  components:{
+    TodoVue
+  }
+}
 </script>
 
 <template>
   <main>
-
+    <TodoVue/>
 </main>
 </template>
