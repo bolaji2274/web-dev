a5367100dccf0b90ebf743b173c538476960abf6
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-11-07 09:38:35 +0100

    changes made

--- vue-app-todo/src/components/Todo.vue
+++ vue-app-todo/src/components/Todo.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <h1>Todo-List App</h1>
+        <form action="" id="new-task">
+            <input type="text" id="new-input"> 
+        </form>
+        <input type="sudmit" value="Add-task">
     </div>
 </template>
 <script>
