e1a7140fee90dcac3cea11ad23bf13f163306136
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-11-08 06:53:46 +0100

    changes mades to voice.vue

--- vue-voice-ai/src/components/Voice.vue
+++ vue-voice-ai/src/components/Voice.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        
+            <h1 style="text-align: center;">Hi welcome voice ai</h1>
+                <div class="heart"></div>
+            <div class="btn">
+                <button id="start">Voice Start</button>
+                <button id="stop">Voice Stop</button>
+                <button id="speak">Speak out</button>
+            </div>
     </div>
 </template>
 
@@ -10,5 +16,31 @@ export default {
 }
 </script>
 <style scoped>
-
+.heart{
+    position: relative;
+    height: 160px;
+    width: 100px;
+    margin: 0 auto;
+    background-color: #d16ba5;
+    border-radius: 200px 200px 0 0;
+    transform: rotate(45deg);
+}
+.heart::before{
+    content: "";
+    position: absolute;
+    background: #d16ba5;
+    height: 160px;
+    width: 100px;
+    transform: rotate(-90deg);
+    border-radius: 200px 200px 0 0;
+    left: -30px;
+    top: 30px;
+}
+.btn{
+    margin-left: 40%;
+    margin-top: 10%;
+    padding: 10px;
+    letter-spacing: 4px;
+    text-transform: uppercase;
+}
 </style>
