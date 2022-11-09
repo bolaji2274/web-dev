9867bae93ac66fe169866f1102c69951c2d6c5f4
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-11-09 06:58:33 +0100

    one commit for todolist

--- vue-app-todo/src/components/TodoList.vue
+++ vue-app-todo/src/components/TodoList.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        
+        <TodoItemVue/>
     </div>
 </template>
 
 <script>
+import TodoItemVue from './TodoItem.vue';
+
 export default{
-    
+components: {
+    TodoItemVue
+},
+name: 'todo-list',
 }
 </script>
