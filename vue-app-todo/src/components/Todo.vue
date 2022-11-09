f0c3909a6ef0b4125e290fe384f43daf50a5146c
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-11-09 02:25:00 +0100

    One align item

--- vue-app-todo/src/components/Todo.vue
+++ vue-app-todo/src/components/Todo.vue
@@ -3,9 +3,13 @@
        <h1>Todo-List App</h1>
      <form>
         <label for="">Title</label>
+        <br>
         <input type="text" class="input-title" required>
+        <br>
         <label for="">Description</label>
+        <br>
         <input type="text" class="desc">
+        <br>
         <button class="btn">Submit</button>
      </form>
     </div>
@@ -19,11 +23,39 @@ export default {
 <style scoped>
 div {
     display: block;
-    text-align: center;
+    /* text-align: center; */
+    align-items: center;
 }
 form{
-    margin: 15px;
-    display: inline-block;
+    font-size: 25px;
 }
-
+input{
+    height: 100px;
+    width: 400px;
+}
+label{
+    text-align: left;
+}
+.input-title{
+    height: 80px;
+}
+input[type=text], select {
+  /* width: 100px; */
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+.btn {
+  width: 80px;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
 </style>
