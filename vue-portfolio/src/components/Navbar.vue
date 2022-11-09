9a072a1676fb7571f338f0b26c05eb9b1ffc72a6
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-11-09 15:16:21 +0100

    Navbar styling addded

--- vue-portfolio/src/components/Navbar.vue
+++ vue-portfolio/src/components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
+    <div class="nav-container">
         <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#"><span class="p-s">P</span> <span class="j-s">J</span> Studio</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a class="nav-link active" aria-current="page" href="#">Home</a>
         <a class="nav-link" href="#">About</a>
@@ -14,6 +14,9 @@
         <a class="nav-link">Pricing</a>
       </div>
     </div>
+    <ul class="nav justify-content-end">
+        <button type="button" class="btn btn-light">Light</button> 
+</ul>
   </div>
 </nav>
 
@@ -27,4 +30,24 @@ export default {
 </script>
 <style scoped>
 
+.nav-container{
+    max-width: 80%;
+    margin: 0 auto;
+}
+.navbar {
+    font-size: 20px;
+}
+.navbar-brand{
+    font-size: 25px;
+}
+.p-s{
+    color: black;
+}
+.j-s{
+    color: blue;
+}
+.btn{
+    border-radius: 30px;
+    border: 2px solid '$purple-100';
+}
 </style>
