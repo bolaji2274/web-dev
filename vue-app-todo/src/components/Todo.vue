34b82b472d73848a37a4185ff42d15118e503634
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-11-09 01:15:39 +0100

    file modifies

--- vue-app-todo/src/components/Todo.vue
+++ vue-app-todo/src/components/Todo.vue
@@ -1,27 +1,6 @@
 <template>
     <div>
-        <header>
-        <h1>Todo-List App</h1>
-        <form action="" id="new-task-form">
-            <input type="text" id="new-task-input"> 
-            <input type="sudmit" id="new-task-submit" value="Add-task">
-        </form>
-    </header>
-
-    <main>
-        <section class="task-list">
-            <h2>Tasks</h2>
-            <div id="tasks">
-                <div class="content">
-                    <input class="text" type="text" value="my shin task">
-                </div>
-                <div class="actions">
-                    <button class="edit">EDIT</button>
-                    <button class="delete">DELETE</button>
-                </div>
-            </div>
-        </section>
-    </main>
+       
     </div>
 </template>
 <script>
