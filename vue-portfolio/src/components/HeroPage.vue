0327c585c4d7f3ae367affc0bb7f379cefeb306c
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-11-11 21:33:13 +0100

    files Heropage modifies

--- vue-portfolio/src/components/HeroPage.vue
+++ vue-portfolio/src/components/HeroPage.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <p>speeed up communication</p>
+        <h1>Hi I'm Bolaji Hammed Adekunle</h1>
     </div>
 </template>
 
@@ -11,5 +12,15 @@ export default {
 </script>
 
 <style scoped>
-
+div{
+    margin-left: 10%;
+    margin-top: 10%;
+}
+div p{
+    color: red;
+    width: 100px;
+}
+div h1{
+    width: 300px;
+}
 </style>
