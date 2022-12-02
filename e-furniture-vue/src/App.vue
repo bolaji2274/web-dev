8ffbeab4abf85d075ab607945799b20065bcbf4f
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-12-02 12:36:01 +0100

    One commit added

---
+++ e-furniture-vue/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+  <header>
+    <Navbar/>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ e-furniture-vue/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Nav Bar </h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+</style>
