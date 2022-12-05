aa61b388702617e17661d8887aa8d78e94f7cd75
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-12-05 11:30:14 +0100

    one commit added

--- e-furniture-vue/src/components/HeroPage.vue
+++ e-furniture-vue/src/components/HeroPage.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="hero-btn">
-    <h1>Sofa Set with Lounger</h1>
+  <div>
+    <div class="container-fluid spacing_enable">
+      <h1>Sofa Set with Lounger</h1>
     <p>Reveal Yourself THROUGH YOUR CHOICE</p>
-
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
   </div>
 </template>
 
@@ -12,6 +14,11 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.container-fluid{
+  background: url('../../images/furni-slideshow_1.webp') no-repeat fixed center;
+  height: 80vh;
+  width: 100%;
+}
 
 </style>
