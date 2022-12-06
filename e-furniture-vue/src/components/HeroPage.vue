71336c774e6b1d1d7677ea80bb37e8d6069c4aac
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-12-06 14:09:06 +0100

    HeroPage modifies

--- e-furniture-vue/src/components/HeroPage.vue
+++ e-furniture-vue/src/components/HeroPage.vue
@@ -5,25 +5,27 @@
         <div class="text-content">
           <h1>Sofa Set with Lounger</h1>
           <p>Reveal Yourself THROUGH YOUR CHOICE</p>
-          <button type="submit" class="myBtn">Shop Now</button>
+          <button type="submit" class="myBtn">SHOP NOW</button>
         </div>
     </div>
   </div>
   <div id="card">
     <div class="card-content">
-      <div class="container">
+      <div class="containers">
         <div class="row">
           <div class="colum-1">
-
-          </div>
-          <div class="colum-1">
-            <h1>Hello Bolaji</h1>
+            <div class="text">
+              <p>Relax with yourself</p>
+              <h1>Coffe Tables</h1>
+              <p>Lorem ipsum sit armet, consectur adispscing elit.</p>
+              <button type="submit" class="myBtn">SHOP NOW</button>
+            </div>
         </div>
         <div class="colum-2">
           <h1>Hello World</h1>
         </div>
         <div class="colum-3">
-          <h1>Hello</h1>
+          <h1>Hello here</h1>
         </div>
         </div>
       </div>
@@ -85,5 +87,48 @@ export default {
 .myBtn:hover{
   color: #fff;
   background-color: #6f5948;
+}
+.row{
+  height: 65%;
+  width: 100%;
+  max-width: 90%;
+  margin: 5% auto;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.colum-1{
+  flex-basis: 33%;
+  background-image: url('../../images/img1.webp');
+  background-size: cover;
+  background-position: center;
+}
+.colum-1:hover{
+  display: block;
+  background-color: #6f5948;
+  opacity: 0.6;
+}
+.colum-2{
+  flex-basis: 33%;
+  background-image: url('../../images/img2.webp');
+  background-size: cover;
+  background-position: center;
+}
+.colum-3{
+  flex-basis: 33%;
+  background-image: url('../../images/img3.webp');
+  background-size: cover;
+  background-position: center;
+}
+.text{
+  display: block;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  max-width: 80%;
+  color: #fff;
+  margin: 50% auto;
+  font-size: 12px;
+
 }
 </style>
