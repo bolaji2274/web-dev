35a29b686800baf491f27514caaf260ebc321dd8
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-12-14 08:47:54 +0100

    pharm store added

---
+++ store-pharm/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
+
+</script>
+
+<template>
+  <header>
+    <Navbar/>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+</style>
---
+++ store-pharm/src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <H1>Hello Wolrd</H1>
+    <i class="fa facebook"></i>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 border">
+          <h1>Hello World</h1>
+        </div>
+        <div class="col-md-6 border">
+          <h1>Hello World</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
